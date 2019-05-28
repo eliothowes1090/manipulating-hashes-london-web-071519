4ebd15d4fbad4962d2554db706ec00ddb9dba8f7
Eliot Howes <ejhowes@gmail.com>
2019-05-28 21:15:26 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,6 +19,10 @@ def first_challenge
       details.each do |icecream|
         if icecream == favorite_icecream_flavors:
           favorite_icecream_flavors:.first.delete
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
